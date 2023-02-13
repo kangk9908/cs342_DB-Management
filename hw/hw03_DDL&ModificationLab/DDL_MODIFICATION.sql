3be149dfef4474db7e71be2b9e49e5c3828d892c
Kun Kang <kk58@calvin.edu>
2023-02-13 16:40:17 -0500

    sheep lab up to step 7

--- hw/hw03_DDL&ModificationLab/DDL_MODIFICATION.sql
+++ hw/hw03_DDL&ModificationLab/DDL_MODIFICATION.sql
@@ -14,4 +14,40 @@ GO
 --step 4
 CREATE SCHEMA ActiveHerd
 
---step 5
+--step 6
+CREATE TABLE [ActiveHerd].sheep (
+	IdNumber INT NOT NULL PRIMARY KEY,
+	SheepName CHAR(30),
+	BreedCategory CHAR(1),
+	Gender CHAR(1),
+	ShepherdId INT
+);
+
+CREATE TABLE [ActiveHerd].breed (
+	BreedCategory CHAR(20) NOT NULL PRIMARY KEY,
+	BreedDescription VARCHAR(50)
+);
+
+CREATE TABLE [ActiveHerd].sheepShots (
+	IdNumber INT NOT NULL PRIMARY KEY,
+	ShotType CHAR(20),
+	ShotDate DATE
+);
+
+CREATE TABLE [ActiveHerd].shotList (
+	ShotType CHAR(20) NOT NULL PRIMARY KEY,
+	shotDescription VARCHAR(50),
+	dayCycle CHAR(30)
+);
+
+CREATE TABLE [ActiveHerd].injectionList (
+	InjectionType CHAR(20) NOT NULL PRIMARY KEY,
+	injectionDescription VARCHAR(50)
+);
+
+CREATE TABLE [ActiveHerd].shepherd (
+	ShepherdID INT NOT NULL PRIMARY KEY,
+	LastName VARCHAR(20),
+	FirstName VARCHAR(20),
+	ShepherdCertification char(1) NOT NULL
+);
