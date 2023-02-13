29e2f9eb6fe5b01ec01642bc9423f8672264baac
Kun Kang <kk58@calvin.edu>
2023-02-13 14:40:44 -0500

    sheep lab initial push

---
+++ hw/hw03_DDL&ModificationLab/DDL_MODIFICATION.sql
@@ -0,0 +1,17 @@
+-- Kun Kang (2301131)
+-- for CS342 Database Management
+-- Professor Patrick Bailey
+-- DDL & Modification Lab
+
+--step 2
+CREATE DATABASE sheep;
+GO
+
+--step 3
+USE sheep;
+GO
+
+--step 4
+CREATE SCHEMA ActiveHerd
+
+--step 5
